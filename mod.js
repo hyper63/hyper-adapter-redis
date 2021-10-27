6b512225a88bac7eb93821f64ae0fe9b526975d9
Tyler Hall <tyler@hyper.io>
2021-10-27 17:41:34 +0000

    feat(cluster): use separate cluster connect in cluster mode #12

--- mod.js
+++ mod.js
@@ -1,4 +1,4 @@
-import { R, redis } from "./deps.js";
+import { R, redis, redisCluster } from "./deps.js";
 
 import createAdapter from "./adapter.js";
 
@@ -9,22 +9,38 @@ const { mergeRight } = R;
  * @property {string} hostname
  * @property {number?} port - defaults to 6379
  *
+ * @typedef RedisAdapterOptions
+ * @property {{ connect: () => Promise<{}> }?} client
+ * @property {boolean?} cluster - defaults to false
+ *
  * @param {RedisClientArgs} config
+ * @param {RedisAdapterOptions?} options
  * @returns {object}
  */
 export default function RedisCacheAdapter(
   config = {},
-  options = { client: redis, cluster: false },
+  options = {},
 ) {
-  options.client = options.client || redis;
+  options.client = options.client || (options.cluster ? redisCluster : redis);
 
   async function load(prevLoad = {}) {
     // prefer args passed to adapter over previous load
     config = mergeRight(prevLoad, config);
-    const client = await options.client.connect(config);
+
+    let client;
     if (options.cluster) {
-      await client.clusterMeet(config.hostname, config.port);
-      await client.clusterNodes();
+      // redis cluster client
+      client = await options.client.connect({
+        nodes: [
+          {
+            hostname: config.hostname,
+            port: config.port,
+          },
+        ],
+      });
+    } else {
+      // regular redis client
+      client = await options.client.connect(config);
     }
     // create client
     return { client };
