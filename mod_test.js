9793138efa5a5361c518b457e2597e405a83e736
Tyler Hall <tyler@hyper.io>
2022-12-12 15:44:26 +0000

    test: use Deno.test steps for grouping

--- mod_test.js
+++ mod_test.js
@@ -12,35 +12,41 @@ const baseStubClient = {
   scan: resolves(),
 };
 
-Deno.test("validate factory schema", () => {
-  assert(validateFactorySchema(RedisCacheAdapter({}, {
-    client: { connect: resolves({ foo: "bar" }) },
-  })));
-});
+Deno.test("mod", async (t) => {
+  await t.step("validate factory schema", () => {
+    assert(validateFactorySchema(RedisCacheAdapter({}, {
+      client: { connect: resolves({ foo: "bar" }) },
+    })));
+  });
 
-Deno.test("returns a redis client", async () => {
-  const res = await RedisCacheAdapter({}, {
-    client: { connect: resolves({ foo: "bar" }) },
-  }).load();
+  await t.step("load", async (t) => {
+    await t.step("returns a redis client", async () => {
+      const res = await RedisCacheAdapter({}, {
+        client: { connect: resolves({ foo: "bar" }) },
+      }).load();
 
-  assert(res.client);
-  assertEquals(res.client.foo, "bar");
-});
+      assert(res.client);
+      assertEquals(res.client.foo, "bar");
+    });
 
-Deno.test("returns a redis cluster client", async () => {
-  const res = await RedisCacheAdapter({ hostname: "foo", port: 6380 }, {
-    client: {
-      connect: (config) => Promise.resolve(config),
-    },
-    cluster: true,
-  }).load();
+    await t.step("returns a redis cluster client", async () => {
+      const res = await RedisCacheAdapter({ hostname: "foo", port: 6380 }, {
+        client: {
+          connect: (config) => Promise.resolve(config),
+        },
+        cluster: true,
+      }).load();
 
-  assert(res.client);
-  assertEquals(res.client.nodes[0].hostname, "foo");
-  assertEquals(res.client.nodes[0].port, 6380);
-});
+      assert(res.client);
+      assertEquals(res.client.nodes[0].hostname, "foo");
+      assertEquals(res.client.nodes[0].port, 6380);
+    });
+  });
 
-Deno.test("returns an adapter", () => {
-  const res = RedisCacheAdapter().link({ client: baseStubClient })();
-  assert(res.createStore);
+  await t.step("link", async (t) => {
+    await t.step("returns an adapter", () => {
+      const res = RedisCacheAdapter().link({ client: baseStubClient })();
+      assert(res.createStore);
+    });
+  });
 });
