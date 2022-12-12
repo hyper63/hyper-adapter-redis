39b7bffc7c4fe7813ae0a475ed806b64ba134a60
Tyler Hall <tyler@hyper.io>
2022-12-12 18:06:18 +0000

    refactor: use HyperErr from hyper-utils

--- deps.js
+++ deps.js
@@ -3,3 +3,8 @@ export { default as crocks } from "https://cdn.skypack.dev/crocks@0.12.4";
 
 export * as redis from "https://deno.land/x/redis@v0.27.4/mod.ts";
 export * as redisCluster from "https://deno.land/x/redis@v0.27.4/experimental/cluster/mod.ts";
+
+export {
+  HyperErr,
+  isHyperErr,
+} from "https://x.nest.land/hyper-utils@0.1.0/hyper-err.js";
