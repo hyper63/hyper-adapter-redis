c0ae7a0e3fa8191aecc18b5929e168111bb75f87
Tyler Hall <tyler@hyper.io>
2023-05-23 20:44:22 -0400

    refactor: remove nest.land imports

--- deps.js
+++ deps.js
@@ -4,4 +4,7 @@ export { default as crocks } from 'https://cdn.skypack.dev/crocks@0.12.4'
 export * as redis from 'https://deno.land/x/redis@v0.29.3/mod.ts'
 export * as redisCluster from 'https://deno.land/x/redis@v0.29.3/experimental/cluster/mod.ts'
 
-export { HyperErr, isHyperErr } from 'https://x.nest.land/hyper-utils@0.1.0/hyper-err.js'
+export {
+  HyperErr,
+  isHyperErr,
+} from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-utils%40v0.1.0/packages/utils/hyper-err.js'
