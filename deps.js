8c5d3e6418905520783405e326a777c8458ab2fb
Tyler Hall <tyler@hyper.io>
2023-06-05 09:53:40 -0400

    chore: bump deps and test harness

--- deps.js
+++ deps.js
@@ -1,10 +1,10 @@
-export * as R from 'https://cdn.skypack.dev/ramda@0.28.0'
+export * as R from 'https://cdn.skypack.dev/ramda@0.29.0'
 export { default as crocks } from 'https://cdn.skypack.dev/crocks@0.12.4'
 
-export * as redis from 'https://deno.land/x/redis@v0.29.3/mod.ts'
-export * as redisCluster from 'https://deno.land/x/redis@v0.29.3/experimental/cluster/mod.ts'
+export * as redis from 'https://deno.land/x/redis@v0.29.4/mod.ts'
+export * as redisCluster from 'https://deno.land/x/redis@v0.29.4/experimental/cluster/mod.ts'
 
 export {
   HyperErr,
   isHyperErr,
-} from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-utils%40v0.1.0/packages/utils/hyper-err.js'
+} from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-utils%40v0.1.1/packages/utils/hyper-err.js'
