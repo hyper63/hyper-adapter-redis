d52a7322b743af65472c41955ebff2c1f597e704
Tyler Hall <tyler@hyper.io>
2023-11-28 19:27:37 -0500

    chore: bump dev deps and use npm in lieu of skypack

--- deps.js
+++ deps.js
@@ -1,5 +1,5 @@
-export * as R from 'https://cdn.skypack.dev/ramda@0.29.0'
-export { default as crocks } from 'https://cdn.skypack.dev/crocks@0.12.4'
+export * as R from 'npm:ramda@0.29.1'
+export { default as crocks } from 'npm:crocks@0.12.4'
 
 export * as redis from 'https://deno.land/x/redis@v0.32.0/mod.ts'
 export * as redisCluster from 'https://deno.land/x/redis@v0.32.0/experimental/cluster/mod.ts'
