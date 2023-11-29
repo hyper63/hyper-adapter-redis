053b0a2d329bef35bb9586814d4c47771e40aea8
Tyler Hall <tyler@hyper.io>
2023-11-28 19:27:37 -0500

    chore: bump dev deps and use npm in lieu of skypack

--- test/hyper.js
+++ test/hyper.js
@@ -1,6 +1,6 @@
 // Harness deps
-import { default as appExpress } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-app-express%40v1.2.0/packages/app-express/mod.ts'
-import { default as core } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper%40v4.3.0/packages/core/mod.ts'
+import { default as appExpress } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-app-express%40v1.2.1/packages/app-express/mod.ts'
+import { default as core } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper%40v4.3.1/packages/core/mod.ts'
 
 import redis from '../mod.js'
 
