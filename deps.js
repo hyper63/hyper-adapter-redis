7b1d6aec89b81ba5574efffa1c21fe7426eb9dce
Tyler Hall <tyler@hyper.io>
2024-01-10 22:37:40 +0000

    chore: bump deps

--- deps.js
+++ deps.js
@@ -1,10 +1,10 @@
 export * as R from 'npm:ramda@0.29.1'
 export { default as crocks } from 'npm:crocks@0.12.4'
 
-export * as redis from 'https://deno.land/x/redis@v0.32.0/mod.ts'
-export * as redisCluster from 'https://deno.land/x/redis@v0.32.0/experimental/cluster/mod.ts'
+export * as redis from 'https://deno.land/x/redis@v0.32.1/mod.ts'
+export * as redisCluster from 'https://deno.land/x/redis@v0.32.1/experimental/cluster/mod.ts'
 
 export {
   HyperErr,
   isHyperErr,
-} from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-utils%40v0.1.1/packages/utils/hyper-err.js'
+} from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-utils%40v0.1.2/packages/utils/hyper-err.js'
