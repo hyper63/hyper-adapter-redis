35d87ace8471189d2434263548e68723ae5f262f
Tyler Hall <tyler@hyper.io>
2024-01-10 22:48:53 +0000

    fix(listDocs,destroyStore): use trampoline in lieu of recursion to prevent callstack overflow #30

--- adapter.js
+++ adapter.js
@@ -2,7 +2,7 @@ import { crocks, HyperErr, R } from './deps.js'
 import { handleHyperErr } from './utils.js'
 
 const { Async } = crocks
-const { always, append, identity, ifElse, isNil, map, not, compose } = R
+const { always, append, identity, ifElse, isNil, not } = R
 
 /**
  * Create a hyper store cache key, with the given prefix.
@@ -14,13 +14,13 @@ const { always, append, identity, ifElse, isNil, map, not, compose } = R
  *
  * @param {string} prefix - the prefix for the key, typically the hyper cache store name
  * @param {string} key - the key
- * @param {boolean} [noHashSlot] - whether to hashtag the prefix, which will cause all keys
+ * @param {boolean} [hashSlot] - whether to hashtag the prefix, which will cause all keys
  * "within" this prefix to be mapped to the same hash slot. This is useful for multi-key operations
  * performed on a Redis cluster. Defaults to false, which is to say "hashtag the prefix"
  * @returns
  */
-const createKey = (prefix, key, noHashSlot = false) =>
-  `${noHashSlot ? `${prefix}` : `{${prefix}}`}_${key}`
+const createKey = (prefix, key, hashSlot = true) =>
+  `${hashSlot ? `{${prefix}}` : `${prefix}`}_${key}`
 
 const mapTtl = (ttl) =>
   ifElse(
@@ -45,7 +45,7 @@ const mapTtl = (ttl) =>
  *
  * @param {Options} options
  */
-export default function ({ redis, scanCount }) {
+export default function ({ redis, scanCount, hashSlot = true }) {
   // redis commands
   // key: Promise<string>
   const get = Async.fromPromise(redis.get.bind(redis))
@@ -55,13 +55,15 @@ export default function ({ redis, scanCount }) {
   const del = Async.fromPromise(redis.del.bind(redis))
   // cursor, { type, pattern }: Promise<[string, string[]]>
   const scan = Async.fromPromise(redis.scan.bind(redis))
+  // key, key, key: Promise<any[]>
+  const mget = Async.fromPromise(redis.mget.bind(redis))
 
   const index = () => {
     return Promise.resolve(HyperErr({ status: 501, msg: 'Not Implemented' }))
   }
 
   const checkIfStoreExists = (store) => (key) =>
-    get(createKey('store', store, true))
+    get(createKey('store', store, false))
       .chain(
         (_) =>
           _ ? Async.Resolved(key) : Async.Rejected(HyperErr({
@@ -90,7 +92,7 @@ export default function ({ redis, scanCount }) {
    */
   const createStore = (name) =>
     Async.of([])
-      .map(append(createKey('store', name, true)))
+      .map(append(createKey('store', name, false)))
       .map(append('active'))
       .chain((args) => set(...args))
       .bichain(
@@ -106,27 +108,18 @@ export default function ({ redis, scanCount }) {
    * @returns {Promise<object>}
    */
   const destroyStore = (name) =>
-    Async.of(createKey(name, '*'))
+    Async.of(createKey(name, '*', hashSlot))
       // grab all keys belonging to this store
-      .chain((matcher) =>
-        scan(0, { pattern: matcher })
-          .chain(getKeys(scan, matcher))
-      )
+      .chain((matcher) => getKeys(scan, matcher, scanCount))
       .chain(
         ifElse(
           (keys) => keys.length > 0,
-          compose(
-            // wait for keys to be deleted
-            Async.all,
-            // delete each key, sequentially
-            // TODO: see https://github.com/hyper63/hyper-adapter-redis/issues/17
-            map(del),
-          ),
+          deleteKeys(del, scanCount, hashSlot),
           (keys) => Async.of(keys),
         ),
       )
       // Delete the key that tracks the store's existence
-      .chain(() => del(createKey('store', name, true)))
+      .chain(() => del(createKey('store', name, false)))
       .bichain(
         handleHyperErr,
         always(Async.Resolved({ ok: true })),
@@ -138,7 +131,7 @@ export default function ({ redis, scanCount }) {
    * @returns {Promise<object>}
    */
   const createDoc = ({ store, key, value, ttl }) =>
-    Async.of([createKey(store, key)])
+    Async.of([createKey(store, key, hashSlot)])
       .chain(checkIfStoreExists(store))
       // don't allow over-writting of existing keys
       .chain(checkForConflict)
@@ -159,7 +152,7 @@ export default function ({ redis, scanCount }) {
    * @returns {Promise<object>}
    */
   const getDoc = ({ store, key }) =>
-    Async.of(createKey(store, key))
+    Async.of(createKey(store, key, hashSlot))
       .chain(checkIfStoreExists(store))
       .chain(get)
       .chain((v) => {
@@ -184,7 +177,7 @@ export default function ({ redis, scanCount }) {
   const updateDoc = ({ store, key, value, ttl }) =>
     Async.of([])
       .chain(checkIfStoreExists(store))
-      .map(append(createKey(store, key)))
+      .map(append(createKey(store, key, hashSlot)))
       .map(append(JSON.stringify(value)))
       .map(mapTtl(ttl))
       .chain((args) => set(...args))
@@ -200,7 +193,7 @@ export default function ({ redis, scanCount }) {
    */
   const deleteDoc = ({ store, key }) =>
     checkIfStoreExists(store)()
-      .chain(() => del(createKey(store, key)))
+      .chain(() => del(createKey(store, key, hashSlot)))
       .bichain(
         handleHyperErr,
         always(Async.Resolved({ ok: true })),
@@ -211,11 +204,9 @@ export default function ({ redis, scanCount }) {
    * @param {CacheQuery}
    * @returns {Promise<object>}
    */
-  const listDocs = async ({ store, pattern = '*' }) => {
-    const matcher = createKey(store, pattern)
-    return await scan(0, { pattern: matcher, count: scanCount })
-      .chain(getKeys(scan, matcher, scanCount))
-      .chain(getValues(get, store))
+  const listDocs = ({ store, pattern = '*' }) => {
+    return getKeys(scan, createKey(store, pattern, hashSlot), scanCount)
+      .chain(getValues({ get, mget }, store, scanCount, hashSlot))
       .bichain(
         handleHyperErr,
         (docs) => Async.Resolved({ ok: true, docs }),
@@ -236,21 +227,124 @@ export default function ({ redis, scanCount }) {
 }
 
 function getKeys(scan, matcher, count) {
-  return function repeat([cursor, keys]) {
-    return cursor === '0' ? Async.Resolved(keys) : scan(cursor, { pattern: matcher, count })
-      .chain(repeat)
-      .map((v) => keys.concat(v))
+  function page(cursor, keys) {
+    return scan(cursor, { pattern: matcher, count })
+      .map(([nCursor, nKeys]) => {
+        keys = keys.concat(nKeys)
+
+        return nCursor === '0'
+          ? keys
+          /**
+           * Return a thunk that continues the next iteration, thus ensuring the callstack
+           * is only ever one call deep.
+           *
+           * This is continuation passing style, to be leverage by our trampoline
+           */
+          : () => page(nCursor, keys)
+      }).toPromise()
+  }
+
+  /**
+   * Our initial thunk that performs the first scan
+   */
+  return Async.fromPromise(page)(0, [])
+    .chain(Async.fromPromise(trampoline))
+}
+
+function getValues({ get, mget }, store, count, hashSlot) {
+  const prefix = `${createKey(store, '', hashSlot).split('_').shift()}_`
+
+  return function (keys) {
+    function page(keys, values) {
+      const nKeys = keys.splice(0, count)
+      return Async.of()
+        /**
+         * If the adapter is NOT configured to use hash slot for the store,
+         * then we must get the keys individually see https://github.com/hyper63/hyper-adapter-redis/issues/17
+         *
+         * Otherwise, we are using hash slots, so all keys for a store will be mapped
+         * to a single hash slot and so can be retrieved as a set.
+         *
+         * Regardless, we still break up the operations into "pages" according
+         * to the provided count, so as to not block the Redis thread for too long
+         * on any given operation
+         */
+        .chain(() => hashSlot ? mget(...nKeys) : Async.all(nKeys.map((key) => get(key))))
+        .map((nValues) => {
+          values = values.concat(
+            nValues.map((v, i) => ({
+              key: nKeys[i].replace(prefix, ''),
+              value: JSON.parse(v),
+            })),
+          )
+
+          return keys.length === 0
+            ? values
+            /**
+             * Return a thunk that continues the next iteration, thus ensuring the callstack
+             * is only ever one call deep.
+             *
+             * This is continuation passing style, to be leverage by our trampoline
+             */
+            : () => page(keys, values)
+        }).toPromise()
+    }
+
+    /**
+     * Our initial thunk that performs the first scan
+     */
+    return Async.fromPromise(page)(keys, [])
+      .chain(Async.fromPromise(trampoline))
   }
 }
 
-function getValues(get, store) {
+function deleteKeys(del, count, hashSlot) {
   return function (keys) {
-    return Async.all(
-      map((key) =>
-        get(key).map((v) => ({
-          key: key.replace(`${store}_`, ''),
-          value: JSON.parse(v),
-        })), keys),
-    )
+    function page(keys) {
+      const nKeys = keys.splice(0, count)
+      return Async.of()
+        /**
+         * If the adapter is NOT configured to use hash slot for the store,
+         * then we must del the keys individually see https://github.com/hyper63/hyper-adapter-redis/issues/17
+         *
+         * Otherwise, we are using hash slots, so all keys for a store will be mapped
+         * to a single hash slot and so can be deleted as a set.
+         *
+         * Regardless, we still break up the operations into "pages" according
+         * to the provided count, so as to not block the Redis thread for too long
+         * on any given operation
+         */
+        .chain(() => hashSlot ? del(...nKeys) : Async.all(nKeys.map((key) => del(key))))
+        .map(() => {
+          return keys.length === 0
+            ? []
+            /**
+             * Return a thunk that continues the next iteration, thus ensuring the callstack
+             * is only ever one call deep.
+             *
+             * This is continuation passing style, to be leverage by our trampoline
+             */
+            : () => page(keys)
+        }).toPromise()
+    }
+
+    /**
+     * Our initial thunk that performs the first scan
+     */
+    return Async.of(keys)
+      .chain(Async.fromPromise(page))
+      .chain(Async.fromPromise(trampoline))
   }
+}
+
+async function trampoline(init) {
+  let result = init
+  /**
+   * Call toPromise() on the Async, to unwrap each iteration
+   * instead of chaining them in a single Async.
+   *
+   * This prevents overflowing the callback, and gives us our trampoline
+   */
+  while (typeof result === 'function') result = await result()
+  return result
 }
